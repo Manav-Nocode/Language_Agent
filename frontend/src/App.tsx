a83e85ebb9065de9337838d09878c8116e054fa2
Manav <manavjarout@gmail.com>
2026-05-29 20:47:07 +0530

    basic setup fe be

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="bg-amber-300 w-full h-screen flex  justify-center items-center">
+      <div className="bg-white h-screen w-[70%] justify-center">
+        {" "}
+        main area{" "}
+      </div>
+    </div>
+  );
+};
+
+export default App;
