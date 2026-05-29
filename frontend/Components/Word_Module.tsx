371fa24f84d60f11e9a48d935704595161bfce1e
Manav <manavjarout@gmail.com>
2026-05-29 22:54:38 +0530

    Basic layout and routing

---
+++ frontend/Components/Word_Module.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Word_Module = () => {
+  return (
+    <div className="p-10">
+      <div className="w-full h-30 rounded-sm bg-orange-300 flex items-center justify-between p-10 mb-4">
+        <span>Word name</span>
+        <span>Modules</span>
+      </div>
+      <div className="bg-blue-300 w-full h-60">Random examples</div>
+    </div>
+  );
+};
+
+export default Word_Module;
